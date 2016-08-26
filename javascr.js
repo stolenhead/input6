a68bb95b90d00fedef4f477d52353bb5cd577969
ANDREA <andrea.palomino.ruby@gmail.com>
2016-08-25 17:13:50 -0700

    new rep

---
+++ javascr.js
@@ -0,0 +1,7 @@
+var num1 = parseInt(prompt("Teclea un numero "));
+  if(num1%2==0){
+           document.write(" numero Par"); 
+           }   
+  else{
+      window.alert("numero Impar");
+      }  
